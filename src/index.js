6c866f5d686fe24d041636c69d6460f4cdf4eca4
Divyansh Chahar <divyanshchahar@outlook.com>
2023-03-18 17:43:25 +0530

    added root div

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import header from "./layouts/header";
 
 const body = document.querySelector("body");
+const rootDiv = document.createElement("div");
+
+rootDiv.setAttribute("id", "root");
 
 header(body);
+
+body.append(rootDiv);
