5d1873291a5eb53f80506a30aad5d711ebaba201
Divyansh Chahar <divyanshchahar@outlook.com>
2023-03-18 15:33:34 +0530

    resolved multiple outputs

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   module: {
     rules: [
@@ -28,9 +29,4 @@ module.exports = {
       title: "Output Management",
     }),
   ],
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
 };
