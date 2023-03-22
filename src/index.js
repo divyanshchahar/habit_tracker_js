97aa6a3e772fe41dd9db750a749bf016c3d6f3c8
Divyansh Chahar <divyanshchahar@outlook.com>
2023-03-22 12:47:38 +0530

    integrated new layout

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import header from "./layouts/header";
 import datePicker from "./componenets/datePicker";
+import inputform from "./layouts/inputform";
+import minMaxDate from "./utils/minMaxDate";
 
 import "./index.css";
 
 const body = document.querySelector("body");
 const rootDiv = document.createElement("div");
+const addButton = document.createElement("button");
 
 rootDiv.setAttribute("id", "root");
 
+addButton.innerText = "+";
+
+addButton.classList.add("btn");
+
+addButton.addEventListener("click", () => {
+  inputform(rootDiv);
+});
+
 header(body);
+
+rootDiv.append(addButton);
+
+datePicker(rootDiv);
 
 body.append(rootDiv);
 
-datePicker(rootDiv);
+minMaxDate();
