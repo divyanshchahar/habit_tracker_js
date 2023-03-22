e98127d7d3faa5f28da50612354e0c9eb8691b6c
Divyansh Chahar <divyanshchahar@outlook.com>
2023-03-22 18:03:13 +0530

    added habit list layout

--- src/layouts/habits.js
+++ src/layouts/habits.js
@@ -1,4 +1,7 @@
 import deleteHabit from "../utils/deleteHabit";
+import pickerLikeDate from "../utils/pickerLikeDate";
+import minMaxDate from "../utils/minMaxDate";
+import editHabit from "../utils/editHabit";
 
 function habits(parentElement) {
   if (document.querySelector("#habit-list")) {
@@ -8,6 +11,8 @@ function habits(parentElement) {
   const habitData = JSON.parse(window.localStorage.getItem("HABIT_DATA"));
 
   if (habitData) {
+    const [minDate, maxDate] = minMaxDate();
+
     const span = document.createElement("span");
 
     span.setAttribute("id", "habit-list");
@@ -15,17 +20,70 @@ function habits(parentElement) {
     habitData.map((item) => {
       const div = document.createElement("div");
       const p = document.createElement("p");
+      const input = document.createElement("input");
       const deleteButton = document.createElement("button");
+      const editButton = document.createElement("button");
+      const startDate = document.createElement("input");
+      const endDate = document.createElement("input");
+      const doneButton = document.createElement("button");
 
       div.setAttribute("id", item.id);
+      input.setAttribute("type", "text");
+      input.setAttribute("id", "edit-habit");
+      input.setAttribute("value", item.name);
+      startDate.setAttribute("type", "date");
+      startDate.setAttribute("id", "edit-start-date");
+      startDate.setAttribute("name", "start-date");
+      startDate.setAttribute("value", pickerLikeDate(item.startDate));
+      startDate.setAttribute("min", minDate);
+      startDate.setAttribute("max", maxDate);
+      endDate.setAttribute("type", "date");
+      endDate.setAttribute("id", "edit-end-date");
+      endDate.setAttribute("name", "end-date");
+      endDate.setAttribute("value", pickerLikeDate(item.endDate));
+      endDate.setAttribute("min", minDate);
+      endDate.setAttribute("max", maxDate);
+
       p.innerText = item.name;
       deleteButton.innerText = "delete";
+      editButton.innerText = "edit";
+      doneButton.innerText = "SAVE";
+
+      input.classList.toggle("hidden");
+      startDate.classList.toggle("hidden");
+      endDate.classList.toggle("hidden");
+      doneButton.classList.toggle("hidden");
 
       deleteButton.addEventListener("click", (e) => {
         deleteHabit(e);
       });
+
+      editButton.addEventListener("click", (e) => {
+        p.classList.toggle("hidden");
+        startDate.classList.toggle("hidden");
+        endDate.classList.toggle("hidden");
+        input.classList.toggle("hidden");
+        e.target.classList.toggle("hidden");
+        doneButton.classList.toggle("hidden");
+      });
+
+      doneButton.addEventListener("click", (e) => {
+        editHabit(e);
+        p.classList.toggle("hidden");
+        startDate.classList.toggle("hidden");
+        endDate.classList.toggle("hidden");
+        input.classList.toggle("hidden");
+        e.target.classList.toggle("hidden");
+        editButton.classList.toggle("hidden");
+      });
+
       div.append(p);
+      div.append(input);
+      div.append(startDate);
+      div.append(endDate);
       div.append(deleteButton);
+      div.append(editButton);
+      div.append(doneButton);
       span.append(div);
     });
     parentElement.append(span);
