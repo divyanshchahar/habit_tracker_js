1b8328339fce37d1329d7fe701792429e214b1d4
Divyansh Chahar <divyanshchahar@outlook.com>
2023-03-24 18:21:43 +0530

    refactored habits.js - added docstring - changed variable name - replaced toggle method with add at some places - rearranged code - changed name attribute

--- src/layouts/habits.js
+++ src/layouts/habits.js
@@ -3,7 +3,11 @@ import pickerLikeDate from "../utils/pickerLikeDate";
 import minMaxDate from "../utils/minMaxDate";
 import editHabit from "../utils/editHabit";
 
-function habits(parentElement) {
+/**
+ * Function to render habits dialog box
+ * @param {HTMLElement} parentContainer - HTML Element which will be parent to the UI
+ */
+function habits(parentContainer) {
   if (document.querySelector("#habit-list")) {
     document.querySelector("#habit-list").remove();
   }
@@ -30,35 +34,37 @@ function habits(parentElement) {
 
       div.setAttribute("id", item.id);
       div.classList.add("edit-habit-field");
+
       input.setAttribute("type", "text");
       input.setAttribute("id", "edit-habit");
       input.setAttribute("value", item.name);
+      input.classList.add("hidden");
+
       startDate.setAttribute("type", "date");
       startDate.setAttribute("id", "edit-start-date");
-      startDate.setAttribute("name", "start-date");
+      startDate.setAttribute("name", "edit-start-date");
       startDate.setAttribute("value", pickerLikeDate(item.startDate));
       startDate.setAttribute("min", minDate);
       startDate.setAttribute("max", maxDate);
+      startDate.classList.add("hidden");
+
       endDate.setAttribute("type", "date");
       endDate.setAttribute("id", "edit-end-date");
-      endDate.setAttribute("name", "end-date");
+      endDate.setAttribute("name", "edit-end-date");
       endDate.setAttribute("value", pickerLikeDate(item.endDate));
       endDate.setAttribute("min", minDate);
       endDate.setAttribute("max", maxDate);
+      endDate.classList.add("hidden");
 
       p.innerText = item.name;
-      deleteButton.innerText = "delete";
-      editButton.innerText = "edit";
-      doneButton.innerText = "SAVE";
 
-      input.classList.toggle("hidden");
-      startDate.classList.toggle("hidden");
-      endDate.classList.toggle("hidden");
-      doneButton.classList.toggle("hidden");
+      deleteButton.innerText = "delete";
 
       deleteButton.addEventListener("click", (e) => {
         deleteHabit(e);
       });
+
+      editButton.innerText = "edit";
 
       editButton.addEventListener("click", (e) => {
         p.classList.toggle("hidden");
@@ -68,6 +74,9 @@ function habits(parentElement) {
         e.target.classList.toggle("hidden");
         doneButton.classList.toggle("hidden");
       });
+
+      doneButton.innerText = "SAVE";
+      doneButton.classList.add("hidden");
 
       doneButton.addEventListener("click", (e) => {
         editHabit(e);
@@ -88,7 +97,7 @@ function habits(parentElement) {
       div.append(doneButton);
       span.append(div);
     });
-    parentElement.append(span);
+    parentContainer.append(span);
     span.showModal();
   }
 }
