414911d9bab71c9c4a7e7f121752679e6427e164
Divyansh Chahar <divyanshchahar@outlook.com>
2023-03-25 08:02:28 +0530

    refactored utcLikeDate.js
    - edited docstring

--- src/utils/utcLikeDate.js
+++ src/utils/utcLikeDate.js
@@ -1,7 +1,7 @@
 /**
  * Converts date from YYYY-MM-DD format to YYYY-M-D i.e. 2023-09-01 get convered to 2023-9-1
  * @param {String} dateString - Date in the format YYYY-MM-DD i.e. 2023-09-117
- * @returns
+ * @returns {String} date in the format YYYY-M-D
  */
 function utcLikeDate(dateString) {
   const [year, month, date] = dateString.split("-");
