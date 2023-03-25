b6990c45d96c82151b22527ea1ab519f0d490f55
Divyansh Chahar <divyanshchahar@outlook.com>
2023-03-25 07:59:51 +0530

    refactored renderDayReport.js
    - added docstring
    - changed variable name

--- src/utils/renderDayReport.js
+++ src/utils/renderDayReport.js
@@ -1,12 +1,18 @@
 import dayReport from "../layouts/dayReport";
 
-function renderDayReport(habitStatus, parentElement, date) {
+/**
+ * Function to render `dayReport`
+ * @param {String} habitStatus - string containing `habitScore`
+ * @param {HTMLElement} parentContainer - root HTML elemet which will be parent to the UI
+ * @param {Date} date - date whose report has to be rendered
+ */
+function renderDayReport(habitStatus, parentContainer, date) {
   if (habitStatus === "0/0") {
     alert("No habits scheduled for this day");
     return;
   }
 
-  dayReport(parentElement, date);
+  dayReport(parentContainer, date);
 }
 
 export default renderDayReport;
